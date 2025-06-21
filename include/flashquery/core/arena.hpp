0186f4ad3662ff75272c2d515cdeb0f9ac5af00a
gaailiunas <gailiunaslaurynas@tutanota.com>
2025-06-21 18:26:09 +0300

    feat: growing arena

---
+++ include/flashquery/core/arena.hpp
@@ -0,0 +1,71 @@
+#ifndef FLASHQUERY_CORE_ARENA_HPP
+#define FLASHQUERY_CORE_ARENA_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <new>
+#include <type_traits>
+#include <utility>
+
+namespace flashquery {
+
+class Arena {
+public:
+    explicit Arena(const std::size_t sz)
+    {
+        this->memory = static_cast<char *>(std::malloc(sz));
+        if (!this->memory)
+            throw std::bad_alloc();
+        this->memory_size = sz;
+        this->offset = 0;
+    }
+
+    ~Arena()
+    {
+        std::free(this->memory);
+    }
+
+    template <typename T, std::size_t N = 1, typename... Args> T *alloc(Args &&... args)
+    {
+        static_assert(std::is_trivially_destructible_v<T>);
+
+        char *unaligned = this->memory + this->offset;
+        T *aligned = this->align_ptr_up<T>(unaligned);
+        std::size_t new_offset = static_cast<char *>(aligned) - this->memory + alignof(T) * N;
+
+        if (new_offset > this->memory_size) {
+            std::size_t new_size = this->memory_size * 2;
+            if (new_offset > new_size)
+                new_size = new_offset;
+            char *tmp = static_cast<char *>(std::realloc(this->memory, new_size));
+            if (!tmp)
+                throw std::bad_alloc();
+            this->memory = tmp;
+            this->memory_size = new_size;
+        }
+
+        this->offset = new_offset;
+
+        for (std::size_t i = 0; i < N; i++)
+            new (aligned + i) T(std::forward<Args>(args)...);
+
+        return aligned;
+    }
+
+private:
+    char *memory;
+    std::size_t memory_size;
+    std::size_t offset;
+
+    template <typename T> constexpr inline T *align_ptr_up(T *ptr) noexcept
+    {
+        std::uintptr_t ptr_val = reinterpret_cast<std::uintptr_t>(ptr);
+        std::uintptr_t aligned_val = (ptr_val + alignof(T) - 1) & ~(alignof(T) - 1);
+        return reinterpret_cast<T *>(aligned_val);
+    }
+};
+
+} // namespace flashquery
+
+#endif // FLASHQUERY_CORE_ARENA_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 int main(void)
 {
     return 0;
